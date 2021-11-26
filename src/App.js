ce7ac19866b990754ff427debd506142bb9d2036
Heba Farestveit Úlfarsdóttir <heba.farestveit@gmail.com>
2021-11-26 11:09:34 +0000

    Update weather info. Fix error.

--- src/App.js
+++ src/App.js
@@ -1,118 +1,63 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Weather from './components/Weather';
 import Forecast from './components/Forecast';
 
 export default function App() {
-  const [latitude, setLatitude] = useState([]);
-  const [longitude, setLongitude] = useState([]);
-  const [weatherData, setWeatherData] = useState([]);
+  const [city, setCity] = useState('');
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [sunrise, setSunrise] = useState(null);
+  const [sunset, setSunset] = useState(null);
+  const [iconId, setIconId] = useState('');
+  const [feelsLike, setFeelsLike] = useState(null);
   const [forecast, setForecast] = useState([]);
-  const [error, setError] = useState(null);
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+    console.log('Latitude is: ', position.coords.latitude);
+    console.log('Longitude is: ', position.coords.longitude);
+  });
 
   useEffect(() => {
-    
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-      console.log('Latitude is: ', position.coords.latitude);
-      console.log('Longitude is: ', position.coords.longitude);
-    });
-
-    getWeather(latitude, longitude)
-    .then(weather => {
-      setWeatherData(weather);
-      setError(null);
-    })
-    .catch(err => {
-      setError(err.message);
-    });
-
-    getForecast(latitude, longitude)
-    .then(data => {
-      setForecast(data);
-      setError(null);
-    })
-    .catch(err => {
-      setError(err.message);
+    axios.get(`${process.env.API_URL}?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&APPID=${process.env.API_KEY}`)
+      .then((weatherData) => {
+        console.log(weatherData.data);
+        setCity(weatherData.data.timezone);
+        setTemperature(weatherData.data.current.temp);
+        setHumidity(weatherData.data.current.humidity);
+        setSunrise(weatherData.data.current.sunrise);
+        setSunset(weatherData.data.current.sunset);
+        setIconId(weatherData.data.current.weather[0].id);
+        setFeelsLike(weatherData.data.current.feels_like);
+        setForecast(weatherData.data.daily);
     });
 // eslint-disable-next-line
-  }, [latitude, longitude, error]);
-
-  function handleResponse(response) {
-    if (response.ok) {
-      return response.json();
-      
-    } else {
-      throw new Error("Please Enable your Location in your browser!");
-    }
-  }
-
-  function getWeather(latitude, longitude) {
-    return fetch(
-      `${process.env.API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env._API_KEY}`
-    )
-    .then(res => handleResponse(res))
-    .then(weather => {
-      if (Object.entries(weather).length) {
-        const mappedData = mapDataToWeatherInterface(weather);
-        console.log('Mapped Data: ', mappedData);
-        return mappedData;
-      }
-    });
-  }
-
-  function getForecast(latitude, longitude) {
-    return fetch(
-      `${process.env.API_URL}/forecast/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.API_KEY}`
-    )
-      .then(res => handleResponse(res))
-      .then(forecastData => {
-        if (Object.entries(forecastData).length) {
-          return forecastData.list
-            .filter(forecast => forecast.dt_txt.match(/09:00:00/))
-            .map(mapDataToWeatherInterface);
-        }
-      });
-  }
-
-  function mapDataToWeatherInterface(data) {
-    const mapped = {
-      date: data.dt * 1000, // convert from seconds to milliseconds
-      description: data.weather[0].main,
-      temperature: Math.round(data.main.temp),
-    };
-  
-    // Add extra properties for the five day forecast: dt_txt, icon, min, max
-    if (data.dt_txt) {
-      mapped.dt_txt = data.dt_txt;
-    }
-  
-    return mapped;
-  }
+  }, [latitude, longitude]);
   
   return (
     <>
       <Header />
-        {(typeof weatherData.main != 'undefined') ? (
-          <main className="main">
-            <Weather weatherData={weatherData} />
-            <Forecast forecast={forecast} weatherData={weatherData} />
-          </main>
-        
-        ) : (
+      <main className="main">
+        <Weather 
+          city={city}
+          temperature={temperature}
+          humidity={humidity}
+          sunrise={sunrise}
+          sunset={sunset}
+          feelsLike={feelsLike}
+          icon={iconId}
+        />
+        <Forecast 
+          forecast={forecast} 
+        />
+      </main>
 
-          <div>
-            <div className="active visible">
-              <div className="content">
-                <div className="ui loader">Loading...</div>
-              </div>
-            </div>
-            <img src="/images/wireframe/short-paragraph.png" className="ui image" alt=""/>
-          </div>
-
-        )}  
     </>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Card, Feed } from 'semantic-ui-react';
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
@@ -12,29 +13,10 @@ import {
 	faSmog  
 } from '@fortawesome/free-solid-svg-icons';
 
-export default function Weather (
-	{weatherData}
-) {
+export default function WeatherCard (
+	{temperature, humidity, city, sunrise, sunset, feelsLike, icon}) {
 
-	const [city, setCity] = useState('');
-	const [temperature, setTemperature] = useState(null);
-	const [humidity, setHumidity] = useState(null);
-	const [sunrise, setSunrise] = useState(null);
-	const [sunset, setSunset] = useState(null);
-	const [iconId, setIconId] = useState('');
-	const [feelsLike, setFeelsLike] = useState(null);
-
-	console.log(weatherData.data);
-    setCity(weatherData.data.timezone);
-    setTemperature(weatherData.data.current.temp);
-    setHumidity(weatherData.data.current.humidity);
-    setSunrise(weatherData.data.current.sunrise);
-    setSunset(weatherData.data.current.sunset);
-    setIconId(weatherData.data.current.weather[0].id);
-    setFeelsLike(weatherData.data.current.feels_like);
-	
 	let weatherIcons = null;
-	const icon = iconId;
 
 	if (icon >= 200 && icon <= 232) {
 		weatherIcons = <FontAwesomeIcon icon={faBolt} size="lg" color="#212121" />
@@ -61,54 +43,49 @@ export default function Weather (
 	}
 
 	return (
-		<div className="weather-card">
-
-			<div className="weather-card__header">
-				<h2>{city}</h2>
-				<span className="icon-container">{weatherIcons}</span>
-			</div>
-
-			<div className="weather-card__body">
-				<div className="weather-card__body__header">
-					<h3>Today</h3>
+		<Card className="weather-card">
+			<Card.Content className="weather-card__header">
+				<Card.Header className="weather-card__child">{city}</Card.Header>
+				<div className="icon-container">
+					{weatherIcons}
 				</div>
-
-				<div className="weather-card__body__date">
-					<p>{moment().format('dddd')}</p>
-					<p>{moment().format('MMM Do')}</p>
-					<p>{moment().format('HH:mm')}</p>
-				</div>
-
-				<div className="weather-card__body__info">
-					<div className="weather-card__body__info--temp">
-						<p>Temperature</p>
-						<p>{Math.round(temperature)} °C</p>
-					</div>
-					
-					<div className="weather-card__body__info--humid">
-						<p>Humidity</p>
-						<p>{humidity} %</p>
-					</div>
-				</div>
-				
-				<div className="weather-card__body__info">
-					<div className="weather-card__body__info--sunrise">
-						<p>Sunrise</p>
-						<p>{new Date(sunrise * 1000).toLocaleTimeString('en-GB')}</p>
-					</div>
-					<div className="weather-card__body__info--sunset">
-						<p>Sunset</p>
-						<p>{new Date(sunset * 1000).toLocaleTimeString('en-GB')}</p>
-					</div>
-				</div>
-
-				<div className="weather-card__body__info">
-					<div className="weather-card__body__info--air-temp">
-						<p>Feels like:</p>
-						<p>{Math.round(feelsLike)} °C</p>
-					</div>
-				</div>
-			</div>
-		</div> 
+			</Card.Content>
+			<Card.Content className="weather-card__body">
+				<Feed>
+					<Feed.Event>
+						<Feed.Content>
+							<h3 className="weather-card__body__child">Today</h3>
+							<p>{moment().format('dddd')}, {(moment().format('MMMM Do, h:mm a'))}</p>
+							<div className="weather-card">
+								<div className="weather-card__body__child">
+									<p>Temperature</p>
+									<p>{Math.round(temperature)} °C</p>
+								</div>
+								<div className="weather-card__body__child">
+									<p>Humidity</p>
+									<p>{humidity} %</p>
+								</div>
+							</div>
+							<div className="weather-card">
+								<div className="weather-card__child">
+									<p>Sunrise</p>
+									<p>{new Date(sunrise * 1000).toLocaleTimeString('en-GB')}</p>
+								</div>
+								<div className="weather-card__child">
+									<p>Sunset</p>
+									<p>{new Date(sunset * 1000).toLocaleTimeString('en-GB')}</p>
+								</div>
+							</div>
+							<div className="weather-card">
+								<div className="weather-card__child">
+									<p>Feels like:</p>
+									<p>{Math.round(feelsLike)} °C</p>
+								</div>
+							</div>
+						</Feed.Content>
+					</Feed.Event>
+				</Feed>
+			</Card.Content>
+		</Card> 
 	)
 }
